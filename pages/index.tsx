d52dab127116802aa59427477f3605c81a746920
Joel Bergman <joel.h.bergman@gmail.com>
2022-01-12 05:10:21 +0200

    Update front page texts

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,13 +16,13 @@ const Home = () => {
 
       <div className="my-8">
         <h2>Crafting profits</h2>
-        <p>Select your server/datacenter & search a craftable item by name.</p>
+        <p>Select your world & search a craftable item by name.</p>
       </div>
 
       <h2>Coming Soon ™️</h2>
       <ul className="list-disc pl-8">
-        <li>Tomestone mats value comparsion</li>
-        <li>White & Purple scrips value comparsion</li>
+        <li>Tomestone material gil value comparsion</li>
+        <li>White & Purple scrips gil value comparsion (Materias vs reagents etc.)</li>
       </ul>
     </Layout>
   );
